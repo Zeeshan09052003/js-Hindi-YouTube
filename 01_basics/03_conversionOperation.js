132a47cec8a1add186ea921ee63116e70084b1c0
Zeeshan09052003 <mohd.zeeshan@advarra.com>
2025-03-03 22:23:30 +0000

    conversions in Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,35 @@ let someNumber = 33
 let stringSomeNumber = String(someNumber)
 console.log(stringSomeNumber);
 console.log(typeof stringSomeNumber);
+
+// ************************************Operation********************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Zeeshan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true); // Not recommended
+console.log(+""); // Not recommended
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // not recommended
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
