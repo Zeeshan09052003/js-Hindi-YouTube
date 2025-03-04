c1daf5d22df1e41880aa434e3d2843f92e2ceeaf
Zeeshan09052003 <mohd.zeeshan@advarra.com>
2025-03-04 21:06:04 +0000

    datatypes summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 1); // treating null as 0
+
+// Same behaviour for undefined 
+// Comparision should always be between same datatype
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// how data is getting kept in the memory and access it is: primitive and non-primitive
+// Primitive
+/*
+7 types:
+String
+Number
+Boolean
+Null
+Undefined
+Symbol : to make any value unique : Ex: const id = Symbol('123');
+BigInt : too big value in numbers : const bigNumber = 45353432432643623432465325236345n
+*/
+
+// Reference (non-primitive)
+/*
+Arrays
+Objects
+Functions
+*/
+
+// Java script is dynamic typed language
+
